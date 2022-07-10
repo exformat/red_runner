73df36596c7e529c2d13f5c746bbf8b784bc392c
exformat <exformat1988@gmail.com>
2022-07-10 15:47:46 +0600

    ...

--- desktop/src/com/exformatgames/defender/DesktopLauncher.java
+++ desktop/src/com/exformatgames/defender/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.exformatgames.defender;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.exformatgames.defender.game.SkedushGame;
+import com.exformatgames.defender.game.RedRunnerGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		new Lwjgl3Application(new SkedushGame(), config);
+		new Lwjgl3Application(new RedRunnerGame(), config);
 	}
 }
